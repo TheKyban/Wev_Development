b73c2f274d34bc2112816cc9fe5f0123485c4fb6
Aditya <aaditya1392@gmail.com>
2023-02-15 19:35:10 +0530

    Calculator

---
+++ Projects/Calculator/Scripts/script.js
@@ -0,0 +1,17 @@
+let string = "";
+let button = document.querySelectorAll(".button");
+Array.from(button).forEach((button)=> {
+    button.addEventListener('click',(e)=>{
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string
+        }else if (e.target.innerHTML == 'C') {
+            string = "";
+            document.querySelector('input').value = string
+        }else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string
+        }
+    })
+})
